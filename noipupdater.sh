6f9e54401692cd972d9e8c14db20d759e8daca55
Emiliano <emics@users.noreply.github.com>
2016-06-21 17:47:52 +0200

    Update noipupdater.sh
    change for  using on dd-wrt router

--- noipupdater.sh
+++ noipupdater.sh
@@ -1,30 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-# No-IP uses emails as passwords, so make sure that you encode the @ as %40
-USERNAME=username
+USERNAME=youremail@gmail.com
 PASSWORD=password
-HOST=hostsite
-LOGFILE=logdir/noip.log
-STOREDIPFILE=configdir/current_ip
-USERAGENT="Simple Bash No-IP Updater/0.4 antoniocs@gmail.com"
+HOST=yourhost.no-ip.biz
+LOGFILE=/root/logs/noip.log
+STOREDIPFILE=/root/logs/current_ip
+USERAGENT="Simple Bash No-IP Updater/0.5 $USERNAME"
 
-if [ ! -e $STOREDIPFILE ]; then 
-	touch $STOREDIPFILE
+if [ ! -e $STOREDIPFILE ]; then
+        touch $STOREDIPFILE
 fi
 
-NEWIP=$(wget -O - http://icanhazip.com/ -o /dev/null)
+
+NEWIP=$(wget -O -  http://www.mioip.it/ )
 STOREDIP=$(cat $STOREDIPFILE)
 
 if [ "$NEWIP" != "$STOREDIP" ]; then
-	RESULT=$(wget -O "$LOGFILE" -q --user-agent="$USERAGENT" --no-check-certificate "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
+        RESULT=$(wget -q -U "$USERAGENT" -O -  "http://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
 
-	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
-	echo $NEWIP > $STOREDIPFILE
+        LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
+        echo $RESULT
+        echo $NEWIP > $STOREDIPFILE
 else
-	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] No IP change"
+        LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] No IP change"
 fi
 
 echo $LOGLINE >> $LOGFILE
 
 exit 0
-
